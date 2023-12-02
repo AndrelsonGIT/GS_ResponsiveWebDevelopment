dd7750ab118565b01fa82c67ee43192a203ffdd7
AndrelsonGIT <148652838+AndrelsonGIT@users.noreply.github.com>
2023-12-01 21:59:09 -0300

    Add chatbot

--- script/script.js
+++ script/script.js
@@ -9,6 +9,20 @@ let buttonTheme = document.querySelector("#toggleButton")
 //     menuMobile.style.display = (menuMobile.style.display === "flex") ? "none" : "flex";
 // }
 
+window.watsonAssistantChatOptions = {
+    integrationID: "d58663cb-a23d-46d9-8515-3fa7cdc063a2", // The ID of this integration.
+    region: "us-east", // The region your integration is hosted in.
+    serviceInstanceID: "a4b6d0c8-aa69-465c-bd4b-0e9a05057fc1", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
+
+
+
 buttonTheme.addEventListener('click', changeColor)
 
 document.addEventListener('DOMContentLoaded', ()=>{
